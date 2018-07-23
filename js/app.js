e6c8707950b0cebf69c31971ef14d8ba1d30f2ca
Jessica Tirado <jessicasoto7@gmail.com>
2018-07-23 15:16:23 -0500

    Cleaned up code and added comments.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-
 // Enemies our player must avoid
-var Enemy = function(x,y) {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
-    this.speed = 100 *(Math.random()*5);
+    this.speed = 100 * (Math.random() * 5);
     this.x = x;
     this.y = y;
     // The image/sprite for our enemies, this uses
@@ -18,11 +17,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-   this.x = this.x + this.speed*dt;
+    this.x = this.x + this.speed * dt;
 
-   if(this.x > 505){
-      this.x = 0;
-    }    
+    if (this.x > 505) {
+        this.x = 0;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -33,57 +32,58 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function (x, y) {
-	this.sprite = 'images/char-pink-girl.png';
-	this.x = x;
-	this.y = y;
+var Player = function(x, y) {
+    this.sprite = 'images/char-pink-girl.png';
+    this.x = x;
+    this.y = y;
 };
 
+//Lives in game
 var heart = document.querySelector(".hearts");
+
+//Temp Array for lives as they are lost.
 var livesUsed = [];
 
-
 Player.prototype.update = function() {
-	allEnemies.forEach(enemy => {
-		if(this.y === enemy.y && 
-		   this.x < enemy.x + 83 &&
-   		   this.x + 83 > enemy.x &&
-   		   this.y < enemy.y + 101 &&
-   		  101 + this.y >enemy.y) {
-			heart.removeChild(heart.lastElementChild);
-		    livesUsed.push(heart);
-		    this.x = 200;
-  			this.y = 405; 			
-		}
-		if (livesUsed.length == 3) {
-			console.log("You lost");
-    		alert("You lost, sorry!");
-    		location.reload();
-		}
-	});
-}
-
-function losingGame() {
-	
+    allEnemies.forEach(enemy => {
+        if (this.y === enemy.y &&
+            this.x < enemy.x + 83 &&
+            this.x + 83 > enemy.x &&
+            this.y < enemy.y + 101 &&
+            101 + this.y > enemy.y) {
+        	//When collision occurs, remove a life.
+            heart.removeChild(heart.lastElementChild);
+            //Push a heart into the array.
+            livesUsed.push(heart);
+            //Reset player to try again.
+            this.x = 200;
+            this.y = 405;
+        }
+        //If there are 3 lives in the array, Game is over.
+        if (livesUsed.length == 3) {
+            alert("You lost, sorry!");
+            location.reload();
+        }
+    });
 };
 
 Player.prototype.render = function() {
-	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.handleInput = function(input) {
-	if (input === 'left' && this.x > 0) {
-		this.x -= 101;
-	} else if (input == 'up' && this.y > 0) {
-		this.y -= 83;
-	} else if (input == 'right' && this.x < 405) {
-		this.x += 101;
-	} else if (input == 'down' && this.y < 405) {
-		this.y += 83;
-	} 
-	if (this.y < 0) {
-		alert("Congrats, you made it to the other side! Lets play again!");
-		location.reload();
+    if (input == 'left' && this.x > 0) {
+        this.x -= 101;
+    } else if (input == 'up' && this.y > 0) {
+        this.y -= 83;
+    } else if (input == 'right' && this.x < 405) {
+        this.x += 101;
+    } else if (input == 'down' && this.y < 405) {
+        this.y += 83;
+    }
+    if (this.y < 0) {
+        alert("Congrats, you made it to the other side! Lets play again!");
+        location.reload();
     }
 };
 
